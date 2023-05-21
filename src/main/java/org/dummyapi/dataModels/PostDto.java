acce2cc26ce84b79b0eb04a38aee53a6b3c5a8e5
Michal.Zelga <michal.zelga@pl.ey.com>
2023-05-21 21:50:39 +0200

    US-18 Crated data model for Post

---
+++ src/main/java/org/dummyapi/dataModels/PostDto.java
@@ -0,0 +1,15 @@
+package org.dummyapi.dataModels;
+
+import java.util.List;
+
+public class PostDto {
+
+    private String id;
+    private String text;
+    private String image;
+    private int likes;
+    private String link;
+    private List<String> tags;
+    private String publishDate;
+//    private
+}
