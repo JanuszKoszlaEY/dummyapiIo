69981adef579a7f485708311a88f739c320e6474
Aleksandra Mikszuta <Aleksandra.Mikszuta@gds.ey.com>
2023-05-22 21:54:37 +0200

    request logging filter added to request configuration + initial response configuration created

--- src/main/java/org/dummyapi/configuration/RequestConfiguration.java
+++ src/main/java/org/dummyapi/configuration/RequestConfiguration.java
@@ -1,49 +1,57 @@
 package org.dummyapi.configuration;
+
 import io.restassured.RestAssured;
 import io.restassured.config.LogConfig;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SessionConfig;
 
+import io.restassured.filter.Filter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RequestConfiguration {
 
     private RestAssuredConfig config;
-    private LogConfig getLogConfigWithAllLogs()
-    {
-        return LogConfig.logConfig()
-                .enablePrettyPrinting(true)
-                .enableLoggingOfRequestAndResponseIfValidationFails();
+
+    private void setLogConfigToAllLogs() {
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 
-    private LogConfig getLogConfigWithoutLogs()
-    {
-        return LogConfig.logConfig().enablePrettyPrinting(true);
+    private void setLogConfigToRequestLogsOnly() {
+        RestAssured.filters(new RequestLoggingFilter());
     }
 
-    enum ReqLogLevel {NONE, ALL}
+    private void setLogConfigToNoLogs() {
+        RestAssured.filters();
+    }
 
-    public RequestConfiguration(ReqLogLevel logLevel)
-    {
-        LogConfig logConfig = null;
-        switch (logLevel)
-        {
+    enum ReqLogLevel {NONE, ALL, REQ}
+
+    public RequestConfiguration(ReqLogLevel logLevel) {
+        switch (logLevel) {
             case ALL: {
-                logConfig = getLogConfigWithAllLogs();
+                setLogConfigToAllLogs();
                 break;
             }
             case NONE: {
-                logConfig = getLogConfigWithoutLogs();
+                setLogConfigToNoLogs();
+                break;
+            }
+            case REQ: {
+                setLogConfigToRequestLogsOnly();
                 break;
             }
         }
         RestAssured.baseURI = "http://dummyapi.io";
         this.config = RestAssured.config()
-                .sessionConfig(new SessionConfig().sessionIdName("sessionId"))
-                .logConfig(logConfig);
+                .sessionConfig(new SessionConfig().sessionIdName("sessionId"));
     }
 
-    public Response get(String body){
+    public Response get(String body) {
         return RestAssured.with().config(this.config).body(body).get();
     }
 }
---
+++ src/main/java/org/dummyapi/configuration/ResponseConfiguration.java
@@ -0,0 +1,15 @@
+package org.dummyapi.configuration;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matchers;
+
+public class ResponseConfiguration {
+    private final static long TIME_OUT = 5000;
+    public static ResponseSpecification get() {
+        ResponseSpecBuilder responseBuilder = new ResponseSpecBuilder();
+        responseBuilder.expectStatusCode(Matchers.anyOf(Matchers.equalTo(200))).expectResponseTime(Matchers.lessThanOrEqualTo(TIME_OUT));
+        return responseBuilder.build();
+    }
+}
