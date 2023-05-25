e1e52865665cd1af4c855ba50c18661b406d3c34
Aleksandra Mikszuta <Aleksandra.Mikszuta@gds.ey.com>
2023-05-25 20:43:06 +0200

    tag request created

--- src/main/java/org/dummyapi/requests/Tag.java
+++ src/main/java/org/dummyapi/requests/Tag.java
@@ -1,2 +1,18 @@
-package org.dummyapi.requests;public class Tag {
+package org.dummyapi.requests;
+
+import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.config.SSLConfig;
+import io.restassured.response.Response;
+import org.dummyapi.configuration.DummyApiProperties;
+import org.dummyapi.configuration.PropertiesManager;
+
+public class Tag {
+    public Response getTag(){
+        PropertiesManager.loadProperties();
+        String path = "/tag";
+        String url = DummyApiProperties.API_BASE_URL.getValue() + path;
+        RestAssuredConfig config = RestAssured.config().sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
+        return RestAssured.given().config(config).get(url);
+    }
 }
