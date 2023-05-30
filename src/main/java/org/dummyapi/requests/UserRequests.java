286cc30d0ffa04743caf2e841de92256de022714
Yaroslav Repetskyy <yaroslav.repetskyy@gds.ey.com>
2023-05-30 12:28:55 +0200

    Pretty format for user requests

---
+++ src/main/java/org/dummyapi/requests/UserRequests.java
@@ -0,0 +1,37 @@
+package org.dummyapi.requests;
+
+import io.restassured.response.Response;
+import org.dummyapi.configuration.RequestConfiguration;
+import org.dummyapi.configuration.ResponseConfiguration;
+import org.dummyapi.dataModels.UserDTO;
+import org.dummyapi.endpoints.UserEndpoints;
+
+import static io.restassured.RestAssured.given;
+
+
+public class UserRequests {
+    public Response createUser(UserDTO userDTO) {
+        return given(RequestConfiguration.get().body(userDTO), ResponseConfiguration.get())
+                .post(UserEndpoints.userCreate);
+    }
+
+    public Response getUserById(String id) {
+        return given(RequestConfiguration.get().body(id), ResponseConfiguration.get())
+                .post(UserEndpoints.userByID);
+    }
+
+    public Response getUserList() {
+        return given(RequestConfiguration.get(), ResponseConfiguration.get())
+                .post(UserEndpoints.user);
+    }
+
+    public Response updateUser(String id) {
+        return given(RequestConfiguration.get().body(id), ResponseConfiguration.get())
+                .put(UserEndpoints.userByID);
+    }
+
+    public Response deleteUser(String id) {
+        return given(RequestConfiguration.get().body(id), ResponseConfiguration.get())
+                .delete(UserEndpoints.userByID);
+    }
+}
