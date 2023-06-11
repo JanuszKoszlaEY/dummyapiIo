a4506676d5801c739d159b27d4083dec61c39c73
Aleksandra Mikszuta <Aleksandra.Mikszuta@gds.ey.com>
2023-06-11 20:49:04 +0200

    user test progress

--- src/test/java/org/dummyapi/testUsers/UserTest.java
+++ src/test/java/org/dummyapi/testUsers/UserTest.java
@@ -1,10 +1,14 @@
 package org.dummyapi.testUsers;
 
 import com.google.gson.JsonParser;
+import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.config.SSLConfig;
 import io.restassured.response.Response;
 import org.dummyapi.asserions.UserAsertions;
 import org.dummyapi.configuration.PropertiesManager;
 import org.dummyapi.configuration.RequestConfiguration;
+import org.dummyapi.dataModels.UserDto;
 import org.dummyapi.endpoints.UserEndpoints;
 import org.dummyapi.requests.UserRequests;
 import org.dummyapi.testData.UserTestData;
@@ -12,25 +16,23 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
 
 
 public class UserTest {
     @BeforeTest
-    public static void setup() {
-        PropertiesManager.loadProperties();
-    }
+    public void
    @Test
    public void createUsers(){
         UserRequests userRequests = new UserRequests();
+       RestAssuredConfig config = RestAssured.config().sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
         Response response = userRequests.createUser(UserTestData.getUserTestData());
-
-        JsonParser jsonParser = new JsonParser();
-        String id = jsonParser.parseString(response.body().toString()).getAsJsonObject().get("id").getAsString();
-
+        String id = response.as(UserDto.class).getId();
         UserAsertions.assertThatUserIsCreated(id);
     }
     @Test
     public void getUsers(){
+        useRelaxedHTTPSValidation();
         UserRequests userRequests = new UserRequests();
         Response response = userRequests.getUserList();
         UserAsertions.assertThatResponseCodeIs(response, 200);
@@ -41,7 +43,10 @@ public class UserTest {
                 .spec(RequestConfiguration.get())
                 .header("app-id", "INVALID_API_KEY")
                 .get(UserEndpoints.user);
-
         UserAsertions.assertThatResponseCodeIs(response, 403);
+    }
+    @Test
+    public void updateUser(){
+
     }
 }
