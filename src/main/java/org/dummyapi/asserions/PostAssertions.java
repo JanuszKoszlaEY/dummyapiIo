d0b801e314ad50b9d309662cca0512064d35f471
Michal.Zelga <michal.zelga@pl.ey.com>
2023-06-15 01:03:09 +0200

    US-28-Post-Test-Data

--- src/main/java/org/dummyapi/asserions/PostAssertions.java
+++ src/main/java/org/dummyapi/asserions/PostAssertions.java
@@ -3,11 +3,22 @@ package org.dummyapi.asserions;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.dummyapi.requests.PostRequests;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class PostAssertions {
 
     public static ValidatableResponse assertGivenResponseStatusCode(Response response, int expectedValue){
 
         return response.then().statusCode(expectedValue);
+    }
+
+    public static ValidatableResponse assertGivenPostIsCreated(String id){
+        Response response = PostRequests.getPostByID(id);
+        return  assertGivenResponseStatusCode(response, 200);
+    }
+
+    public static ValidatableResponse assertGivenTextisEqualorLessThan50Char(String id){
+        Response response = PostRequests.getPostByID(id);
+        return response.then().body("text.size()", lessThanOrEqualTo(50));
     }
 }
